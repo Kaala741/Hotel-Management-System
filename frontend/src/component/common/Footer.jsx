e5009983fd880cb9e67ea7604fbaf7a4b06d2b39
Seelam peenaki durga venkata manikanta <138599161+Kaala741@users.noreply.github.com>
2025-10-18 14:03:15 +0530

    adding frontend files

---
+++ frontend/src/component/common/Footer.jsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+                Check In with Saul | All Right Reserved &copy; {new Date().getFullYear()}
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
